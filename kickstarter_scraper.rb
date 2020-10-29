ed333ce030d204247cb32e27a432c1a8eaa5bbb2
Nicholas Merolle <nmerolle11@hotmail.com>
2020-10-29 08:26:31 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,6 +2,8 @@ require "nokogiri"
 require "pry"
 
 def create_project_hash
-  
+  html = File.read('fixtures/kickstarter.html')
+ 
+  kickstarter = Nokogiri::HTML(html)
   # write your code here
 end
